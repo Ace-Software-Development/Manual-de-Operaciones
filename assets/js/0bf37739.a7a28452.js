516823a6a3904ddc55b8f4f0b1e2604c23559551
Pau Cardoso <paulinacardoso7@gmail.com>
2022-02-28 18:35:11 -0600

    Deploy website - based on c40a9bc6dce04406f51544c68615addb5377f8bb

---
+++ assets/js/0bf37739.a7a28452.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmanuales=self.webpackChunkmanuales||[]).push([[745],{8807:function(e){e.exports=JSON.parse('{"permalink":"/Manual-de-Operaciones/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/9db5c1a5.3b12345c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmanuales=self.webpackChunkmanuales||[]).push([[919],{7211:function(a){a.exports=JSON.parse('{"allTagsPath":"/Manual-de-Operaciones/blog/tags","slug":"/Manual-de-Operaciones/blog/tags/facebook","name":"facebook","count":1,"permalink":"/Manual-de-Operaciones/blog/tags/facebook"}')}}]);
