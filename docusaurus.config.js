f509b4a115023d0c97e7be660554a15ae5545d71
AdrianaMillares <69483582+AdrianaMillares@users.noreply.github.com>
2022-03-07 00:16:30 -0600

    Intro site update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ACE Technology. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
